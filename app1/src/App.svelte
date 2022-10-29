2ab53446421824edfa87b8d1a9a3b109f243034f
FernandoLepore <juliofernandolepore@gmail.com>
2022-10-28 23:55:31 -0300

    refactorizando con typescript

--- app1/src/App.svelte
+++ app1/src/App.svelte
@@ -1,30 +1,44 @@
 <script lang="ts">
-	export let name: string;
+  let nombre: string = "amore";
+  let varContador: number = 0;
+
+  const agregar = () => {
+    varContador++;
+  };
+  const restar = () => {
+    varContador--;
+  };
+  const reset = () => {
+    varContador = 0;
+  };
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Hola {nombre}!</h1>
+  <p>contador: {varContador}</p>
+  <button on:click={agregar}>+1</button>
+  <button on:click={restar}>-1</button>
+  <button on:click={reset}>reset</button>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
